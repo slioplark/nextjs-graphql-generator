736ac30d481dfba87b8aefd865f9fe65249bfe0c
Mason <jeailfy9520@outlook.com>
2022-11-27 17:21:26 +0800

    Initial commit from Create Next App

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react'
+
+interface LayoutProps {
+  children: ReactNode
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return <div>{children}</div>
+}
+
+export default Layout
