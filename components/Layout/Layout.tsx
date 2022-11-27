fb534387d33eef9ea19a4eba4ea782320d7e7428
Mason <jeailfy9520@outlook.com>
2022-11-27 17:27:24 +0800

    feat: add Navbar

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,11 +1,17 @@
 import React, { ReactNode } from 'react'
+import Navbar from './Navbar'
 
 interface LayoutProps {
   children: ReactNode
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return <div>{children}</div>
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  )
 }
 
 export default Layout
